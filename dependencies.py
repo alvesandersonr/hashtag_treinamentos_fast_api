6438959e87ad7c1ea88552d0a95cbd276fe0eb73
Anderson Alves <contato.andersonralves@gmail.com>
2025-08-07 16:20:42 -0300

    Creation JWT Token

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,8 @@
-from sqlalchemy.orm import sessionmaker
-from models import db
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import sessionmaker, Session
+from models import db, Usuario
+from jose import jwt, JWTError
+from main import SECRET_KEY, ALGORITHM, oauth2_schema
 
 def pegar_sessao():
     try:
@@ -8,3 +11,16 @@ def pegar_sessao():
         yield session
     finally:
         session.close()
+
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        usuario_id = int(dic_info.get('sub'))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Acesso Negado, verifique a validade do token")
+    
+    usuario = session.query(Usuario).filter(Usuario.id==usuario_id).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso Inválido")
+    
+    return usuario
--- routes/auth/auth.py
+++ routes/auth/auth.py
@@ -1,11 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
-from dependencies import pegar_sessao
-from main import bcrypt_context
-from schemas import UsuarioSchema
+from dependencies import pegar_sessao, verificar_token
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
+from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+def criar_token(usuario_id, duracao_token= timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {"sub": str(usuario_id), "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
+
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()    
+    if not usuario or not bcrypt_context.verify(senha, usuario.senha):
+        return False    
+    return usuario
 
 @auth_router.get("/")
 async def home():
@@ -27,3 +41,28 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
     session.commit()
     
     return {"message": f"Usuário cadastrado com sucesso {usuario_schema.email}"}
+
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
+    
+    if not usuario:
+        raise HTTPException(status_code=401, detail="E-mail ou senha inválidos")
+    
+    access_token = criar_token(usuario.id)
+    refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
+    
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "Bearer"
+        }
+
+@auth_router.get("/refresh")
+async def use_refresh_token(usuario: Usuario = Depends(verificar_token)):
+    access_token = criar_token(usuario.id)
+    return {
+        "access_token": access_token,
+        "token_type": "Bearer"
+        }
+    
